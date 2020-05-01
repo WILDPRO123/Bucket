d9c27fec9159c8cafb460268b977fb419e30081c
WILD <wildpro6@gmail.com>
2020-05-01 14:02:01 +0500

    version 0.1.0

---
+++ RaspenGames/RaspenGames/Photo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RaspenGames
+{
+    public class Photo
+    {
+        public readonly int width;
+        public readonly int height;
+        private Pixel[,] data;
+
+        public Pixel this[int x, int y]
+        {
+            get
+            {
+                return data[x, y];
+            }
+            set
+            {
+                data[x, y] = data[x, y];
+            }
+        }
+
+
+        public Photo(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            data = new Pixel[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    data[i, j] = new Pixel();
+                }
+            }
+        }
+    }
+}
---
+++ RaspenGames/RaspenGames/Pixel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspenGames
+{
+    public class Pixel
+    {
+       public byte Red;
+       public byte Green;
+       public byte Blue;
+        public Pixel(byte Red,byte Green,byte Blue) 
+        {
+            this.Red = Red;
+            this.Green = Green;
+            this.Blue = Blue;
+        }
+        public override string ToString()
+        {
+            return $"{Red},{Green},{Blue}";
+        }
+    }
+}
